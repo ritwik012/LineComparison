9ebb41dcdf9e2932081f5f93231d33168aec5fed
“ritwik012” <rutwikgulhaneg@gmail.com>
2021-11-11 21:32:23 +0530

    [Rutwik]Added:LengthCalculation LineComparison Features Implemented

---
+++ LineComparison/LengthCalculation.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LineComparision
+{
+    class LineCalculation
+    {
+        double xOnePt, xTwopt, yOnept, yTwopt;
+        public LineCalculation(double xOnePt, double xTwopt, double yOnept, double yTwopt)
+        {
+            this.xOnePt = xOnePt;
+            this.xTwopt = xTwopt;
+            this.yOnept = yOnept;
+            this.yTwopt = yTwopt;
+        }
+        public double lengthCalculation()
+        {
+            double result = Math.Sqrt(Math.Pow(this.xTwopt - this.xOnePt, 2) + (Math.Pow(this.yTwopt - this.yOnept, 2)));
+            return result;
+        }
+        public void Check(double lengthOne, double lengthTwo)
+        {
+            if (lengthOne.CompareTo(lengthTwo) == 0)
+            {
+                Console.WriteLine("Both the Lines are Equals");
+            }
+        }
+    }
+}
---
+++ LineComparison/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace LineComparision
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Line Comparision Computation Program");
+            LineCalculation ln = new LineCalculation(9.0, 1.0, 3.0, 5.0);
+            double lengthOne = ln.lengthCalculation();
+            Console.WriteLine("The Length of the First Line is {0}", lengthOne);
+            double lengthTwo = ln.lengthCalculation();
+            Console.WriteLine("The Length of Second Line is {0}", lengthTwo);
+            ln.Check(lengthOne, lengthTwo);
+        }
+    }
+}
